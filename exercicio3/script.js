c5ab2e5a7f0f3ef9d47f5c01681fe4911d8ca8fc
euleo11 <leonardov.silva15@gmail.com>
2022-08-29 16:31:44 -0300

    Terceira atividade completa

---
+++ exercicio3/script.js
@@ -0,0 +1,9 @@
+let a = 10
+let b = 25
+let c = a
+console.log("O novo valor de a é", a = b) 
+console.log("O novo valor de b é", b = c) 
+
+
+// console.log("O novo valor de b é 10")
+// console.log("O novo valor de a é 25")
